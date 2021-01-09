8eb145f0dcbaab42c23c5524d9c78351022ec3d4
Sergii Afanasiev <sergey.afanasiev@rubygarage.org>
2021-01-10 01:32:38 +0200

    Update accept headers

--- Source/AppAuthCore/OIDTokenRequest.m
+++ Source/AppAuthCore/OIDTokenRequest.m
@@ -270,7 +270,9 @@ static NSString *const kAdditionalParametersKey = @"additionalParameters";
   static NSString *const kHTTPContentTypeHeaderKey = @"Content-Type";
   static NSString *const kHTTPContentTypeHeaderValue =
       @"application/x-www-form-urlencoded; charset=UTF-8";
-
+  static NSString *const kHTTPAcceptHeaderKey = @"Accept";
+  static NSString *const kHTTPAcceptHeaderValue = @"application/json";
+  
   NSURL *tokenRequestURL = [self tokenRequestURL];
   NSMutableURLRequest *URLRequest = [[NSURLRequest requestWithURL:tokenRequestURL] mutableCopy];
   URLRequest.HTTPMethod = kHTTPPost;
@@ -278,7 +280,9 @@ static NSString *const kAdditionalParametersKey = @"additionalParameters";
 
   OIDURLQueryComponent *bodyParameters = [self tokenRequestBody];
   NSMutableDictionary *httpHeaders = [[NSMutableDictionary alloc] init];
-
+  
+  [httpHeaders setObject:kHTTPAcceptHeaderValue forKey:kHTTPAcceptHeaderKey];
+  
   if (_clientSecret) {
     // The client id and secret are encoded using the "application/x-www-form-urlencoded" 
     // encoding algorithm per RFC 6749 Section 2.3.1.
